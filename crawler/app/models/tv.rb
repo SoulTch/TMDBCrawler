4d9e21c4a4c0bc9e0d74576e07469e38e4a005de
SoulTch <soultch@SoulTchui-MacBook-Pro.local>
2020-05-11 16:59:26 +0900

    fixed indents

--- crawler/app/models/tv.rb
+++ crawler/app/models/tv.rb
@@ -1,23 +1,23 @@
 class Tv < SimpleRecord
-	has_many :tv_seasons
-	has_many :tv_episodes
+  has_many :tv_seasons
+  has_many :tv_episodes
 
-	has_many :tv_credits
-	has_many :people, through: :tv_credits
+  has_many :tv_credits
+  has_many :people, through: :tv_credits
 
-	set_crawl "/tv/%d", (Proc.new do |obj, json|
-		json["seasons"].each do |col| 
-			TvSeason.crawl [obj["id"], col["season_number"]], [obj.tv_seasons]
-	end
-	end)
-	set_crawl_all 	"tv_series"
-	set_update "tv"
+  set_crawl "/tv/%d", (Proc.new do |obj, json|
+    json["seasons"].each do |col| 
+      TvSeason.crawl [obj["id"], col["season_number"]], [obj.tv_seasons]
+    end
+  end)
+  set_crawl_all 	"tv_series"
+  set_update "tv"
 
 
-	def to_s
-		return "#{name}"
-	end
-	def to_col
-		return {link: "/tv/#{id}", content: to_s}
-	end
+  def to_s
+    return "#{name}"
+  end
+  def to_col
+    return {link: "/tv/#{id}", content: to_s}
+  end
 end
